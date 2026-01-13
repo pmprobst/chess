87b27e962714f8b35acf2bd1fe82396413030c7c
Paul Probst <167970555+pmprobst@users.noreply.github.com>
2026-01-13 14:34:45 -0700

    finished drafting resetBoard to place rest of pieces

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -66,5 +66,73 @@ public class ChessBoard {
                 squares[row][col] = null;
             }
         }
+
+        //position pawns
+        for (int col = 1; col <= 8; col++) {
+            addPiece(new ChessPosition(2, col),
+                    new ChessPiece(ChessGame.TeamColor.WHITE,
+                            ChessPiece.PieceType.PAWN));
+            addPiece(new ChessPosition(7, col),
+                    new ChessPiece(ChessGame.TeamColor.BLACK,
+                            ChessPiece.PieceType.PAWN));
+        }
+
+        //add rooks
+        addPiece(new ChessPosition(1,1),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(1,8),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8,1),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.ROOK));
+        addPiece(new ChessPosition(8,8),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.ROOK));
+
+        //add knights
+        addPiece(new ChessPosition(1, 2),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(1, 7),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 2),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.KNIGHT));
+        addPiece(new ChessPosition(8, 7),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.KNIGHT));
+
+        //add bishops
+        addPiece(new ChessPosition(1, 3),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(1, 6),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 3),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.BISHOP));
+        addPiece(new ChessPosition(8, 6),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.BISHOP));
+
+        // Place queens
+        addPiece(new ChessPosition(1, 4),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.QUEEN));
+        addPiece(new ChessPosition(8, 4),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.QUEEN));
+
+        // Place kings
+        addPiece(new ChessPosition(1, 5),
+                new ChessPiece(ChessGame.TeamColor.WHITE,
+                        ChessPiece.PieceType.KING));
+        addPiece(new ChessPosition(8, 5),
+                new ChessPiece(ChessGame.TeamColor.BLACK,
+                        ChessPiece.PieceType.KING));
     }
 }
