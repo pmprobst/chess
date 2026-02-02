11fb5e8062edc6c459b7cbedbd02b082c330ab94
paul probst <github@paul.probst.org>
2026-02-02 16:01:12 -0700

    Implement isInCheck method in ChessGame to determine if a team is in check using the isInCheckOnBoard method, enhancing game state validation.

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -89,7 +89,7 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        return isInCheckOnBoard(board, teamColor);
     }
 
     /**
